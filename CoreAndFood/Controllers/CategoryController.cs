a884b0fd1b2eb7862fae4dfb8b862756410e7b7c
Huseyin ARKIN <huseyinarkin@outlook.com>
2021-10-22 17:07:03 +0300

    Category ekleme sayfası

--- CoreAndFood/Controllers/CategoryController.cs
+++ CoreAndFood/Controllers/CategoryController.cs
@@ -4,15 +4,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CoreAndFood.Repositories;
+using CoreAndFood.Models;
 
 namespace CoreAndFood.Controllers
 {
     public class CategoryController : Controller
     {
+        CategoryRepository categoryRepository = new CategoryRepository();
         public IActionResult Index()
         {
-            CategoryRepository categoryRepository = new CategoryRepository();            
+                        
             return View(categoryRepository.TList());
+        }
+        [HttpGet]
+        public IActionResult CategoryAdd()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CategoryAdd(Category p)
+        {
+            categoryRepository.TAdd(p);
+            return RedirectToAction("Index");
         }
     }
 }
