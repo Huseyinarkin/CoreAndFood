70a12f1188995a6420ec4eaa8fcc72f897a02ebf
Huseyin ARKIN <huseyinarkin@outlook.com>
2021-11-01 00:46:49 +0300

    google chart

---
+++ CoreAndFood/Controllers/ChartController.cs
@@ -0,0 +1,37 @@
+﻿using CoreAndFood.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAndFood.Controllers
+{
+    public class ChartController : Controller
+    {
+        Context c = new Context();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult VisualizeProductResult()
+        {
+            return Json(FoodList());
+        }
+        public List<foodChart> FoodList()
+        {
+            List<foodChart> fc = new List<foodChart>();
+            using (var c = new Context())
+            {
+                fc = c.Foods.Select(x => new foodChart
+                {
+                    foodName = x.Name,
+                    stock = x.Stock
+                }).ToList();
+            }
+            return fc;
+        }
+
+    }
+}
---
+++ CoreAndFood/Models/foodChart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAndFood.Models
+{
+    public class foodChart
+    {
+        public string foodName { get; set; }
+        public int stock { get; set; }
+    }
+}
